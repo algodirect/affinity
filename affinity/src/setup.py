3b60bc5223d58aea8e55719babaad2e81eecb78a
Ashish Tyagi <2ashishtyagi@gmail.com>
2014-05-18 23:40:40 +0530

    Added setup.py an uploaded the package on PyPI as cpu_affinity.

---
+++ affinity/src/setup.py
@@ -0,0 +1,17 @@
+'''
+Created on May 18, 2014
+
+@author: ashish
+'''
+from distutils.core import setup
+
+setup(
+    name = "cpu_affinity",
+    version = "0.1.0",
+    author = "Ashish Tyagi",
+    author_email = "2ashishtyagi@gmail.com",
+    url = "https://github.com/algodirect/affinity",
+    description = "This is to support set/get CPU affinity in python2.",
+    keywords = "affinity",
+    packages = ["affinity"]
+)
